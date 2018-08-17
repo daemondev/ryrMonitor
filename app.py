b8396a612b4d0a587732dddc9a2b254a4c72c67b
daemondev <granlinux@gmail.com>
2018-08-17 04:53:12 -0500

    add mini-report

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
@@ -8,6 +9,7 @@ import json
 import psycopg2 as pq
 import psycopg2.extras
 import os
+#from datetime import datetime
 
 import shutil
 
@@ -44,10 +46,10 @@ class LoginHandler(BaseHandler):
 ioloop = tornado.ioloop.IOLoop.instance()
 hub = set()
 agents = []
-cnx = pq.connect('host=ryr.homeplex.org port=55443 dbname=asterisk user=asterisk password=$asterisk$123$')
+#cnx = pq.connect('host=ryr.homeplex.org port=55443 dbname=asterisk user=asterisk password=$asterisk$123$')
 
 #-------------------------------------------------- BEGIN [production connect] - (14-07-2018 - 14:03:56) {{
-#cnx = pq.connect('host=172.16.16.9 port=5432 dbname=asterisk user=asterisk password=$asterisk$123$')
+cnx = pq.connect('host=172.16.16.9 port=5432 dbname=asterisk user=asterisk password=$asterisk$123$')
 #-------------------------------------------------- END   [production connect] - (14-07-2018 - 14:03:56) }}
 
 #cnx = pq.connect('host=190.117.161.6 dbname=asterisk user=asterisk password=$asterisk$123$')
@@ -88,12 +90,35 @@ def get_record_file(id):
     except Exception as e:
 	pass
 
-def websocketManager(event, data):
+#class JSONEncoder(json.JSONEncoder):
+    #def default(self, o):
+        #if isinstance(o, datetime):
+            #return o.isoformat()
+        #return json.JSONEncoder.default(self, o)
+
+@gen.coroutine
+def send_agent_stats(callerID, self):
+    query1 = """select count(id) as "Q.CALLS", sum(COALESCE(dtime,0))*'1 second'::interval || '' as "dialedtime",sum(COALESCE(dtime,0) - COALESCE(answeredtime,0))*'1 second'::interval || '' as "ringingtime", sum(COALESCE(answeredtime,0))*'1 second'::interval || '' as "answeredtime", case dialstatus when 'ANSWER' then 'CONTESTADO' when 'CANCEL' then 'CANCELADO' ELSE COALESCE(dialstatus, 'CONGESTION-2') end as "state" from calls where callerid = %d and ins::date = now()::date -1 group by dialstatus order by 1 desc""" % callerID
+    query2 = """select callerid as "Agente", exten as "Teléfono", to_char(ins, 'HH:MI:SS') as "Inicio.Llamada", coalesce(to_char(upd, 'HH:MI:SS'),'') as "Fin.Llamada", coalesce(answeredtime,0) * '1 second'::interval || '' as "Tiempo.Hablado", coalesce(dtime-answeredtime,0) * '1 second'::interval || '' as "Tiempo.Timbrado", coalesce(dtime,0) * '1 second'::interval || '' as "Tiempo.Total", case dialstatus when 'ANSWER' then 'CONTESTADO' when 'CANCEL' then 'CANCELADO' ELSE COALESCE(dialstatus, 'CONGESTION-2') end  as "estado", calltype as "Tipo.Llamada" from calls where callerid = %d and ins::date = now()::date-1 order by ins;""" % callerID
+
+    cursor = cnx.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cursor.execute(query1)
+    stats1 = list(cursor.fetchall());
+
+    cursor.execute(query2)
+    stats2 = list(cursor.fetchall());
+
+    self.write_message(json.dumps({'event': 'showAgentStats', 'data': [stats1, stats2]}))
+
+def websocketManager(event, data, self=None):
     if event == '__get_recorded_file__':
 	print("prepare work for retrieve record file [%s]" % data)
 	#get_record_file(int(data))
     elif event == 'identify_android_device':
         print(">>>> connected android device with: %s" % data)
+    elif event == 'getAgentStats':
+        print(">>> sending stats for agent [%d]" % data)
+        send_agent_stats(data, self)
     else:
 	return
 	print("normal event")
@@ -115,7 +140,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         #cur = cnx.cursor(cursor_factory = psycopg2.extras.DictCursor)
         cur = cnx.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
         #query = "select id,callerid,state,exten,to_char(starttime,'DD/MM/YYYY - HH:MI:SS') as starttime,to_char(endtime,'DD/MM/YYYY - HH:MI:SS') as endtime,to_char(totaltime,'HH:MI:SS') as totaltime,to_char(ringtime,'HH:MI:SS') as ringtime,to_char(answertime,'HH:MI:SS') as answertime,to_char(holdtime,'HH:MI:SS') as holdtime,to_char(day,'DD/MM/YYYY') as day from agent_state where day = now()::date"
-        query = "select id,callerid,state,exten,calltype,(to_char(starttime,'DD/MM/YYYY - HH:MI:SS') is null) as starttime,(to_char(endtime,'DD/MM/YYYY - HH:MI:SS') is null) as endtime,(to_char(totaltime,'HH:MI:SS') is null) as totaltime,(to_char(ringtime,'HH:MI:SS') is null) as ringtime,(to_char(answertime,'HH:MI:SS') is null) as answertime,(to_char(holdtime,'HH:MI:SS') is null) as holdtime,(to_char(day,'DD/MM/YYYY') is null) as day from agent_state where day = now()::date"
+        query = "select id,callerid,state,exten,calltype,(to_char(starttime,'DD/MM/YYYY - HH:MI:SS') is null) as starttime,(to_char(endtime,'DD/MM/YYYY - HH:MI:SS') is null) as endtime,(to_char(totaltime,'HH:MI:SS') is null) as totaltime,(to_char(ringtime,'HH:MI:SS') is null) as ringtime,(to_char(answertime,'HH:MI:SS') is null) as answertime,(to_char(holdtime,'HH:MI:SS') is null) as holdtime,(to_char(day,'DD/MM/YYYY') is null) as day from agent_state where day = now()::date - 1"
         cur.execute(query)
         rows = cur.fetchall()
         #rows = cur.execute(query)
@@ -139,10 +164,10 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         data = raw['data']
 
         print('message received from client', message)
-        websocketManager(event, data)
+        websocketManager(event, data, self)
 
     def on_close(self):
-        print('client disconnecyed:', self)
+        print('client disconnected:', self)
         hub.remove(self)
 
 class AjaxHandler(tornado.web.RequestHandler):
--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,24 @@
 var ws = null;
-var URL = 'ws://localhost:8000/ws';
+//var URL = 'ws://localhost:8000/ws';
 //var URL = 'ws://190.117.161.6:8000/ws';
 //#-------------------------------------------------- BEGIN [production URL] - (14-07-2018 - 14:03:05) {{
-//var URL = 'ws://ryr.progr.am/ws'; //PRODUCTION URL
+var URL = 'ws://ryr.progr.am/ws'; //PRODUCTION URL
 //#-------------------------------------------------- END   [production URL] - (14-07-2018 - 14:03:05) }}
 
 var agents = [];
+var agents_talking = [];
+var agents_ringing = [];
+var agents_idle = [];
 var log_color = {"C": "#89FF28", "E": "#FF2828"};
 
 function setLog(message, color){
     div_log = document.getElementById('div_log');
     div_log.innerHTML = message;
     div_log.style.backgroundColor = log_color[color];
+}
+
+function close_agents_stats(){
+    document.getElementById("div_agent_stats").style.display = 'none';
 }
 
 function onOpen(){
@@ -22,14 +29,23 @@ var state_colors_dict = {"IDLE": "#C7E6F7", "RINGING": "#FCFFC3", "TALKING": "#A
 
 function updateState(data){
     if (agents.indexOf(data['row']['callerid']) < 0) {
-        alert("insert new callerid: " + data['row']['callerid']);
         updateCards(data['row']);
+        resumeAgents();
+        return;
     }
+
     target_exten = document.getElementById(data["row"]["callerid"]+"");
-    target_exten.getElementsByClassName('state')[0].innerHTML = data["row"]['state'];
-    target_exten.getElementsByClassName('calltype')[0].innerHTML = data["row"]['calltype'];
-    target_exten.getElementsByClassName('exten')[0].innerHTML = data["row"]['exten'];
-    target_exten.style.backgroundColor = state_colors_dict[data.row.state];
+
+    if(data["type"] == "DELETE"){
+        target_exten.remove();
+        agents.splice(agents.indexOf(data["row"]["callerid"]), 1);
+        resumeAgents();
+    } else{
+        target_exten.getElementsByClassName('state')[0].innerHTML = data["row"]['state'];
+        target_exten.getElementsByClassName('calltype')[0].innerHTML = data["row"]['calltype'];
+        target_exten.getElementsByClassName('exten')[0].innerHTML = data["row"]['exten'];
+        target_exten.style.backgroundColor = state_colors_dict[data.row.state];
+    }
 }
 
 function displayMessage(data){
@@ -41,17 +57,21 @@ function updateCards(callerID){
     div_dataContainner.innerHTML = div_dataContainner.innerHTML + buildCard(callerID);
 }
 
+function getAgentStats (callerID) {
+    send('getAgentStats', callerID);
+}
+
 function buildCard(caller){
-        return "<div style='background-color:" + state_colors_dict[caller['state']] + ";' class='div_exten' id='" + caller['callerid'] + "'><span>AGENT: " + caller['callerid'] + " STATUS: <strong class='state'>" + caller['state'] + "</strong> CALLTYPE: <span class='calltype'>" + caller['calltype'] + "</span> PHONE: <span class='exten'>" + caller["exten"] + "</span></span></div>";
+        agents.push(caller['callerid']);
+        return "<div style='background-color:" + state_colors_dict[caller['state']] + ";' onclick='getAgentStats(" + caller["callerid"] + ");' class='div_exten' id='" + caller['callerid'] + "'><span>AGENT: " + caller['callerid'] + " STATUS: <strong class='state'>" + caller['state'] + "</strong> CALLTYPE: <span class='calltype'>" + caller['calltype'] + "</span> PHONE: <span class='exten'>" + caller["exten"] + "</span></span></div>";
 }
 
 function populateCards(data){
     card = "<div id='dataContainner'>";
     for (var i = 0; i < data.length; i++) {
-        agents.push(data[i]['callerid']);
-        //card = card + "<div style='background-color:" + state_colors_dict[data[i]['state']] + ";' class='div_exten' id='" + data[i]['callerid'] + "'><span>AGENT: " + data[i]['callerid'] + " STATUS: <strong class='state'>" + data[i]['state'] + "</strong> CALLTYPE: <span class='calltype'>" + data[i]['calltype'] + "</span> PHONE: <span class='exten'>" + data[i]["exten"] + "</span></span></div>";
         card = card + buildCard(data[i]);
     }
+    resumeAgents();
     return card + "</div>";
 }
 
@@ -60,10 +80,35 @@ function fillData (data) {
     div_data.innerHTML = populateCards(data);
 }
 
+function resumeAgents(){
+    document.getElementById("div_total").innerHTML = agents.length;
+}
+
+function showAgentStats(data){
+    div_agent_stats = document.getElementById("div_agent_stats");
+    var table_stats1 = "<div class='center'><table align='center' class='tbl_agent_stats'><tr><th>Q.TOTAL.LLAMADAS</th><th>TIEMPO.TOTAL.LLAMADAS</th><th>TIEMPO.TOTAL.TIMBRADO</th><th>FIN.LLAMADA</th><th>ESTADO</th></tr>";
+
+    data[0].map(function(v, i){
+        table_stats1 = table_stats1 + "<tr><td>" + v["Q.CALLS"] + "</td><td>" + v["dialedtime"] + "</td><td>" + v["ringingtime"] + "</td><td>" + v["answeredtime"] + "</td><td>" + v["state"] + "</td></tr>";
+    });
+
+    table_stats1 = table_stats1 + "</table></br></hr>";
+    var table_stats2 = table_stats1 + "<table align='center' class='tbl_agent_stats'><tr><th>AGENTE</th><th>TEL&Eacute;FONO</th><th>INICIO.LLAMADA</th><th>FIN.LLAMADA</th><th>TIEMPO.HABLADO</th><th>TIEMPO.TIMBRADO</th><th>TIEMPO.TOTAL</th><th>ESTADO</th><th>TIPO.LLAMADA</th></tr>";
+
+    data[1].map(function(v, i){
+        table_stats2 = table_stats2 + "<tr><td>" + v["Agente"] + "</td><td>" + v["Teléfono"] + "</td><td>" + v["Inicio.Llamada"] + "</td><td>" + v["Fin.Llamada"] + "</td><td>" + v["Tiempo.Hablado"] + "</td><td>" + v["Tiempo.Timbrado"] + "</td><td>" + v["Tiempo.Total"] + "</td><td>" + v["estado"] + "</td><td>" + v["Tipo.Llamada"] + "</td></tr>";
+    });
+    table_stats2 = table_stats2 + "</table></div>";
+    div_close = "<div onclick='close_agents_stats();' id='div_close_agent_stats'>&#x2715;</div>";
+    div_agent_stats.innerHTML = div_close + table_stats2;
+    div_agent_stats.style.display = 'block';
+}
+
 handlers = {
 	'updateState': updateState,
 	'displayMessage': displayMessage,
-        'fillData': fillData
+        'fillData': fillData,
+        'showAgentStats': showAgentStats
 }
 
 function onMessage(message){
